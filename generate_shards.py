5888d35f8c84979b692b0c2c7fa2fdd730cc97e7
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2026-06-25 18:28:36 +0530

    Generated shards

--- generate_shards.py
+++ generate_shards.py
@@ -1,2 +1,8 @@
 import webdataset as wds
 from pathlib import Path
+
+images = list(Path('images').glob('*.png'))
+
+with wds.ShardWriter("Shards/shard-%06d.tar", maxcount=500) as sink:
+    for img in images:
+        sink.write({'__key__':img.stem, 'png':img.read_bytes()})
